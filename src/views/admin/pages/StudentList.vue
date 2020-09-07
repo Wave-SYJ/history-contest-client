5dc464f0ef3be5917bbe5cabc8cb4f254b634f26
Wave-SYJ <sun1yu1jia1@outlook.com>
2020-09-07 23:21:34 +0800

    管理端可以查询到分数

--- src/views/admin/pages/StudentList.vue
+++ src/views/admin/pages/StudentList.vue
@@ -34,6 +34,7 @@
             </el-tag>
           </template>
         </el-table-column>
+        <el-table-column prop="score" label="分数"> </el-table-column>
         <el-table-column label="操作" width="175">
           <template slot-scope="scope">
             <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
