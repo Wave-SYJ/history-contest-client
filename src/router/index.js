ad0a8406a2eb437691c2c30bc405a79e419d5512
Wave-SYJ <sun1yu1jia1@outlook.com>
2020-09-13 20:31:35 +0800

    打包优化

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,34 @@ import store from "@/store";
 
 Vue.use(VueRouter);
 
-// const originalPush = VueRouter.prototype.push;
-// VueRouter.prototype.push = function push(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalPush.call(this, location, onResolve, onReject);
-//   return originalPush.call(this, location).catch(err => err);
-// };
+const Login = resolve => require(["@/views/Login.vue"], resolve);
+const Register = resolve => require(["@/views/Register.vue"], resolve);
 
-// const originalReplace = VueRouter.prototype.replace;
-// VueRouter.prototype.replace = function replace(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalReplace.call(this, location, onResolve, onReject);
-//   return originalReplace.call(this, location).catch(err => err);
-// };
+const AdminHome = resolve => require(["@/views/admin/Home.vue"], resolve);
+const StudentHome = resolve => require(["@/views/student/Home.vue"], resolve);
+
+const StudentIndex = resolve =>
+  require(["@/views/student/pages/Welcome.vue"], resolve);
+const Exam = resolve => require(["@/views/student/pages/Exam.vue"], resolve);
+const Details = resolve =>
+  require(["@/views/student/pages/Details.vue"], resolve);
+
+const AdminIndex = resolve =>
+  require(["@/views/admin/pages/Welcome.vue"], resolve);
+const StudentList = resolve =>
+  require(["@/views/admin/pages/StudentList.vue"], resolve);
+const ChoiceList = resolve =>
+  require(["@/views/admin/pages/ChoiceQuestionList.vue"], resolve);
+const JudgeList = resolve =>
+  require(["@/views/admin/pages/JudgeQuestionList.vue"], resolve);
+const ChangePassword = resolve =>
+  require(["@/views/admin/pages/ChangePassword.vue"], resolve);
 
 export const menuList = [
   {
     path: "/admin/index",
     name: "index",
-    component: () => import("@/views/admin/pages/Welcome.vue"),
+    component: AdminIndex,
     meta: {
       title: "主页",
       icon: "el-icon-s-home",
@@ -33,7 +42,7 @@ export const menuList = [
   {
     path: "/admin/students",
     name: "students",
-    component: () => import("@/views/admin/pages/StudentList.vue"),
+    component: StudentList,
     meta: {
       title: "学生列表",
       icon: "el-icon-s-grid",
@@ -43,7 +52,7 @@ export const menuList = [
   {
     path: "/admin/choice",
     name: "choice",
-    component: () => import("@/views/admin/pages/ChoiceQuestionList.vue"),
+    component: ChoiceList,
     meta: {
       title: "选择题列表",
       icon: "el-icon-s-grid",
@@ -53,7 +62,7 @@ export const menuList = [
   {
     path: "/admin/judge",
     name: "judge",
-    component: () => import("@/views/admin/pages/JudgeQuestionList.vue"),
+    component: JudgeList,
     meta: {
       title: "判断题列表",
       icon: "el-icon-s-grid",
@@ -63,7 +72,7 @@ export const menuList = [
   {
     path: "/admin/changePassword",
     name: "changePassword",
-    component: () => import("@/views/admin/pages/ChangePassword.vue"),
+    component: ChangePassword,
     meta: {
       title: "修改密码",
       icon: "el-icon-lock",
@@ -80,18 +89,18 @@ export const routes = [
   {
     path: "/login",
     name: "login",
-    component: () => import("@/views/Login.vue")
+    component: Login
   },
   {
     path: "/register",
     name: "register",
-    component: () => import("@/views/Register.vue")
+    component: Register
   },
   {
     path: "/admin/home",
     name: "admin-home",
     redirect: "/admin/index",
-    component: () => import("@/views/admin/Home.vue"),
+    component: AdminHome,
     children: menuList,
     meta: {
       role: [constants.ROLE_ADMIN]
@@ -101,7 +110,7 @@ export const routes = [
     path: "/student/home",
     name: "student-home",
     redirect: "student/index",
-    component: () => import("@/views/student/Home.vue"),
+    component: StudentHome,
     meta: {
       role: [constants.ROLE_STUDENT],
       status: [
@@ -114,7 +123,7 @@ export const routes = [
     children: [
       {
         path: "/student/index",
-        component: () => import("@/views/student/pages/Welcome.vue"),
+        component: StudentIndex,
         meta: {
           role: [constants.ROLE_STUDENT],
           status: [
@@ -128,7 +137,7 @@ export const routes = [
       {
         path: "/student/exam/:id",
         name: "exam",
-        component: () => import("@/views/student/pages/Exam.vue"),
+        component: Exam,
         meta: {
           role: [constants.ROLE_STUDENT],
           status: [
@@ -141,7 +150,7 @@ export const routes = [
       {
         path: "/student/details",
         name: "details",
-        component: () => import("@/views/student/pages/Details.vue"),
+        component: Details,
         meta: {
           role: [constants.ROLE_STUDENT],
           status: [constants.STATUS_SUBMITTED]
--- vue.config.js
+++ vue.config.js
@@ -5,19 +5,10 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
-const name = "答题"; // 标题
-
-const port = process.env.port || process.env.npm_config_port || 8080; // 端口
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
-  outputDir: "dist",
-  assetsDir: "static",
-  lintOnSave: process.env.NODE_ENV === "development",
-  productionSourceMap: false,
   devServer: {
     host: "0.0.0.0",
-    port: port,
+    port: 8080,
     open: true,
     proxy: {
       [process.env.VUE_APP_BASE_API]: {
@@ -30,12 +21,43 @@ module.exports = {
     },
     disableHostCheck: true
   },
-  configureWebpack: {
-    name: name,
-    resolve: {
-      alias: {
-        "@": resolve("src")
-      }
-    }
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src"));
+
+    config.when(process.env.NODE_ENV !== "development", config => {
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial" // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          vant: {
+            name: "chunk-vant", // split elementUI into a single package
+            priority: 15, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?vant(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
+        }
+      });
+      config.optimization.runtimeChunk("single"),
+        {
+          from: path.resolve(__dirname, "./public/robots.txt"), //防爬虫文件
+          to: "./" //到根目录下
+        };
+    });
   }
 };
